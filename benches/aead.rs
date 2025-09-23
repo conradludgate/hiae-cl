d61f68da123529d6553741da3196e7e116ef295c
Conrad Ludgate <conradludgate@gmail.com>
2025-09-23 10:26:12 +0100

    make bench less cache friendly

--- benches/aead.rs
+++ benches/aead.rs
@@ -1,13 +1,21 @@
+use aead::rand_core::{RngCore, SeedableRng};
 use divan::Bencher;
 use divan::counter::BytesCount;
+use rand_xoshiro::Xoshiro512StarStar;
 
 use std::hint::black_box;
 
 fn main() {
     divan::Divan::from_args()
-        .sample_size(1000)
-        .sample_count(1000)
+        .sample_size(256)
+        .sample_count(512)
         .main();
+}
+
+fn rand(rng: &mut impl RngCore) -> Vec<u8> {
+    let mut vec = vec![0; 1024 * 1024];
+    rng.fill_bytes(&mut vec);
+    vec
 }
 
 #[divan::bench]
@@ -15,16 +23,18 @@ fn hiae_cl(b: Bencher) {
     use aead::{AeadInOut, KeyInit, inout::InOutBuf};
     use hiae_cl::HiAe;
 
-    let mut m = vec![0xd0u8; 16384];
     let key = [0u8; 32];
     let nonce = [0u8; 16];
+    let mut rng = Xoshiro512StarStar::seed_from_u64(1);
 
-    b.counter(BytesCount::of_slice(&m)).bench_local(|| {
-        let state = HiAe::new(&black_box(key).into());
-        state.encrypt_inout_detached(
-            &black_box(nonce).into(),
-            &[],
-            InOutBuf::from(black_box(&mut *m)),
-        )
-    });
+    b.with_inputs(|| rand(&mut rng))
+        .input_counter(|m| BytesCount::of_slice(&m))
+        .bench_local_refs(|m| {
+            let state = HiAe::new(&black_box(key).into());
+            state.encrypt_inout_detached(
+                &black_box(nonce).into(),
+                &[],
+                InOutBuf::from(black_box(&mut **m)),
+            )
+        });
 }
--- benches/mac.rs
+++ benches/mac.rs
@@ -1,13 +1,21 @@
+use aead::rand_core::{RngCore, SeedableRng};
 use divan::Bencher;
 use divan::counter::BytesCount;
+use rand_xoshiro::Xoshiro512StarStar;
 
 use std::hint::black_box;
 
 fn main() {
     divan::Divan::from_args()
-        .sample_size(1000)
-        .sample_count(1000)
+        .sample_size(256)
+        .sample_count(512)
         .main();
+}
+
+fn rand(rng: &mut impl RngCore) -> Vec<u8> {
+    let mut vec = vec![0; 1024 * 1024];
+    rng.fill_bytes(&mut vec);
+    vec
 }
 
 #[divan::bench]
@@ -15,14 +23,16 @@ fn hiae_cl(b: Bencher) {
     use digest::{Mac, crypto_common::KeyIvInit};
     use hiae_cl::HiAeMac;
 
-    let m = vec![0xd0u8; 65536];
     let key = [0u8; 32];
+    let mut rng = Xoshiro512StarStar::seed_from_u64(1);
 
-    b.counter(BytesCount::of_slice(&m)).bench_local(|| {
-        let mut state = HiAeMac::new(&black_box(key).into(), &black_box([0u8; 16].into()));
-        state.update(black_box(&m));
-        state.finalize()
-    });
+    b.with_inputs(|| rand(&mut rng))
+        .input_counter(|m| BytesCount::of_slice(&m))
+        .bench_local_refs(|m| {
+            let mut state = HiAeMac::new(&black_box(key).into(), &black_box([0u8; 16].into()));
+            state.update(m);
+            state.finalize()
+        });
 }
 
 #[divan::bench]
@@ -30,17 +40,19 @@ fn hiae_cl_aead(b: Bencher) {
     use aead::{AeadInOut, KeyInit, inout::InOutBuf};
     use hiae_cl::HiAe;
 
-    let m = vec![0xd0u8; 65536];
     let key = [0u8; 32];
+    let mut rng = Xoshiro512StarStar::seed_from_u64(1);
 
-    b.counter(BytesCount::of_slice(&m)).bench_local(|| {
-        let state = HiAe::new(&black_box(key).into());
-        state
-            .encrypt_inout_detached(
-                &black_box([0u8; 16].into()),
-                black_box(&m),
-                InOutBuf::from(&mut [][..]),
-            )
-            .unwrap()
-    });
+    b.with_inputs(|| rand(&mut rng))
+        .input_counter(|m| BytesCount::of_slice(&m))
+        .bench_local_refs(|m| {
+            let state = HiAe::new(&black_box(key).into());
+            state
+                .encrypt_inout_detached(
+                    &black_box([0u8; 16].into()),
+                    m,
+                    InOutBuf::from(&mut [][..]),
+                )
+                .unwrap()
+        });
 }
